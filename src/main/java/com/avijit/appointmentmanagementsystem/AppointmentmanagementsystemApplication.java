509d5062bc1513cfbd5183d7803d6fbb14b9b522
avisrabon22 <avisrabon22@gmail.com>
2024-04-02 20:45:55 +0530

    first update

---
+++ src/main/java/com/avijit/appointmentmanagementsystem/AppointmentmanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.avijit.appointmentmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentmanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentmanagementsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avijit/appointmentmanagementsystem/AppointmentmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avijit.appointmentmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentmanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
