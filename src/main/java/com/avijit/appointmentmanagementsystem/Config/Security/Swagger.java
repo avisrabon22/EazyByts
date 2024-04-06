832a8fee4930ca48d20475d24a36aa76c8d05786
Avijit Maity <79418104+avisrabon22@users.noreply.github.com>
2024-04-06 18:33:14 +0000

    updates

--- src/main/java/com/avijit/appointmentmanagementsystem/Config/Security/Swagger.java
+++ src/main/java/com/avijit/appointmentmanagementsystem/Config/Security/Swagger.java
@@ -2,6 +2,7 @@ package com.avijit.appointmentmanagementsystem.Config.Security;
 
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -10,11 +11,11 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
+@Configuration
 @EnableSwagger2
 public class Swagger {
     @Bean
-    public Docket api() {
+    Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)).paths(PathSelectors.any())
--- src/main/java/com/avijit/appointmentmanagementsystem/Controllers/SwagerController.java
+++ src/main/java/com/avijit/appointmentmanagementsystem/Controllers/SwagerController.java
@@ -1,17 +1,22 @@
 package com.avijit.appointmentmanagementsystem.Controllers;
 
-
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 import java.io.IOException;
 
 
 @RestController
+@RequestMapping("/")
+@Api
 public class SwagerController {
-    @RequestMapping(value = "/")
+    @GetMapping()
     public void redirect(HttpServletResponse response) throws IOException {
         response.sendRedirect("/swagger-ui.html");
     }
